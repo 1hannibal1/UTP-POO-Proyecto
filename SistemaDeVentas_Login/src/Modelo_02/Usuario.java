04e58da1fa2eacb1d5afc774ddfd436517010922
Denilson Quintana <U19213031@utp.edu.pe>
2024-09-14 11:30:06 -0500

    Denil
    avance

---
+++ SistemaDeVentas_Login/src/Modelo_02/Usuario.java
@@ -0,0 +1,41 @@
+
+package Modelo_02;
+
+public class Usuario {
+     private String codigo;
+    private String nombre;
+    private String clave;
+
+    public Usuario() {
+    }
+
+    public Usuario(String codigo, String nombre, String clave) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
