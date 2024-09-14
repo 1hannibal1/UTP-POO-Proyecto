88fa6e7c01bb3f8d3b99c513a0f9c47f12b6ddfb
Dreize-oss <vanesaaguilar01@gmail.com>
2024-09-14 16:35:03 -0600

    primer intento de Login

--- SistemaDeVentas_Login/src/modelo/UsuarioManager.java
+++ SistemaDeVentas_Login/src/modelo/UsuarioManager.java
@@ -1,27 +1,47 @@
 package modelo;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UsuarioManager {
 
-    private static final ArrayList<Usuario> usuarios;
-
-    static {
-        usuarios = new ArrayList<>();
-        usuarios.add(new Usuario("1", "admin", "admin"));
-        usuarios.add(new Usuario("2", "user", "password"));
-    }
-
-    public boolean validarUsuario(String nombre, String clave) {
-        for (Usuario usuario : usuarios) {
-            if (usuario.getNombre().equals(nombre) && usuario.getClave().equals(clave)) {
-                return true;
-            }
+    public class Login extends Usuario {
+        public Login(String nombre, String usuario, String password) {
+            super(nombre, usuario, password);
         }
-        return false;
+        
+        public void login() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese su usuario:");
+            
+            String usuarioText = scanner.nextLine();
+            
+        }
     }
-
-    public static ArrayList<Usuario> getUsuario() {
-        return usuarios;
+    
+    public class Mozo extends Usuario {
+    private int mesasAsignadas;
+    public Mozo(String nombre, String usuario, String password, int mesasAsignadas) {
+        super(nombre, usuario, password);
+        this.mesasAsignadas = mesasAsignadas;
     }
 }
+    // Clase Gerente que hereda de Usuario
+    public class Gerente extends Usuario {
+    private String Local; 
+    public Gerente(String nombre, String usuario, String password, String Local) {
+        super(nombre, usuario, password);
+        this.Local = Local;
+    }
+
+}
+    // Clase Cajero que hereda de Usuario
+    public class Cajero extends Usuario {
+    private int NumeroCaja;
+    public Cajero(String nombre, String usuario, String password, int NumeroCaja) {
+        super(nombre, usuario, password);
+        this.NumeroCaja = NumeroCaja;
+    }
+    }
+}
+    
