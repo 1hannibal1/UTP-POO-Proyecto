af4d2842d7874e28e2519e981f1a38ae9759ee25
Denilson Quintana <U19213031@utp.edu.pe>
2024-11-28 22:09:07 -0500

    avance
    adasdas

---
+++ SistemaDeVentas_Login/src/modelo/Pedido.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Pedido {
+    private String nombreCliente;
+    private String nombreProducto;
+    private int cantidad;
+    private int numeroMesa;
+    private int precio;
+
+// constructor 
+    public Pedido(String nombreCliente, String nombreProducto, int cantidad, int numeroMesa) {
+        this.nombreCliente = nombreCliente;
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+        this.numeroMesa = numeroMesa;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public int getNumeroMesa() {
+        return numeroMesa;
+    }
+
+    @Override
+    public String toString() {
+        return "Mesa: " + numeroMesa + ", Cliente: " + nombreCliente + ", Producto: " + nombreProducto + ", Cantidad: " + cantidad;
+    }
+}
